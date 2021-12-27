7bbb55370827936b4d0285c11d2df02a2fe5bc1f
b4sh5i <pwner@kakao.com>
2021-12-27 15:53:20 +0900

    [21-12-27] 대충 구현함

--- restAPI/models.py
+++ restAPI/models.py
@@ -1,17 +1,10 @@
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+class b4sh5i_data(models.Model):
+    idx = models.IntegerField(default=0)
+    name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    published_at = models.DateTimeField(blank = True, null = True)
-
-    def publish(self):
-        self.published_at = timezone.now()
-        self.save()
-
+    
     def __str__(self):
-        return self.title
+        return self.name
